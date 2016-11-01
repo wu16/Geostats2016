e7abaaf30e2435fd743d1206e6c80c577fc68bc3
wei wu <wei.wu05@gmail.com>
2016-11-01 08:05:32 -0500

    update mixed model code

--- Class8_10252016/mixedmodel.r
+++ Class8_10252016/mixedmodel.r
@@ -1,4 +1,4 @@
-setwd("f:/Wu/MixedModel/")
+setwd("f:/Wu/geostat2016Fall/geostats2016/Class8_10252016")
 Boreality <- read.table("boreality.txt", header=T)
 Boreality$Bor<-sqrt(1000*(Boreality$nBor+1)/(Boreality$nTot))
 B.lm<-lm(Bor~Wet,data=Boreality)
@@ -19,38 +19,22 @@ plot(Vario1)
 Vario2 <- variogram(E ~ 1, mydata, alpha = c(0, 45, 90,135) )
 plot(Vario2)
 
-
-library(nlme)
-D<-seq(0,1,by=0.1)
-Mydata2<-data.frame(D=D)
-cs1C <- corSpher(c(0.8,0.1), form = ~ D,nugget=T)
-cs1C <- Initialize(cs1C,data=Mydata2)
-v1C<-Variogram(cs1C)
-plot(v1C,smooth=F,type="l",col=1)
-
-
 library(nlme)
 f1 <- formula(Bor ~ Wet)
 B1.gls<-gls(f1, data = Boreality)
-var1<-Variogram(B1.gls,form=~x+y,robust=TRUE,maxDist=2000,
-             resType="pearson")
+var1<-Variogram(B1.gls,form=~x+y,robust=TRUE,maxDist=2000, resType="pearson")
 plot(var1,smooth=T)
 
-
-
 B1A<-gls(f1,correlation=corSpher(form=~x+y,nugget=T),data=Boreality)
-B1B<-gls(f1,correlation=corLin(form=~x+y,nugget=T),data=Boreality)
 B1C<-gls(f1,correlation=corRatio(form=~x+y,nugget=T),data=Boreality)
 B1D<-gls(f1,correlation=corGaus(form=~x+y,nugget=T),data=Boreality)
 B1E<-gls(f1,correlation=corExp(form=~x+y,nugget=T),data=Boreality)
-AIC(B1.gls,B1A,B1B,B1C,B1D,B1E)
+B1F<-gls(f1,correlation=corExp(c(500, 0.8), form=~x+y,nugget=T),data=Boreality)
+AIC(B1.gls,B1A,B1C,B1D,B1E)
 
 
-Vario1E <- Variogram(B1E, form =~ x + y, robust = TRUE, maxDist = 2000,
-                 resType = "pearson")
+Vario1E <- Variogram(B1E, form =~ x + y, robust = TRUE, maxDist = 2000, resType = "pearson")
 plot(Vario1E,smooth=FALSE)
 
-Vario2E <- Variogram(B1E, form =~ x + y,
-                     robust =  TRUE, maxDist = 2000,
-                     resType = "normalized")
+Vario2E <- Variogram(B1E, form =~ x + y, robust =  TRUE, maxDist = 2000, resType = "normalized")
 plot(Vario2E, smooth = FALSE)
